3424f1010eaf5c5781048d8f1577af1566a7f9cd
Mohammad SN <shahnazi2002@gmail.com>
2022-01-07 13:02:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 number = int(input('Enter a number and I will calculate all prime numbers less than or equal to it.'))
 
-numbers = [i for i in range(2,number+1)]
+numbers = [i for i in range(2, number+1)]
 primeNumbers = []
 for i in numbers:
     primeNumbers.append(i)
